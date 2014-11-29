79d3d28074d15c6fd7aa069800ec72636d4d6c2f
Alfred Salahov <alfred.salahov@gmail.com>
2014-11-29 09:34:26 +0300

    Finished 17

---
+++ ex17extra.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+./ex17extra dbextra.dat c
+
+./ex17extra dbextra.dat s 0 annie annie@mrbow.com
+./ex17extra dbextra.dat s 1 zed zed@zedshaw.com
+./ex17extra dbextra.dat s 2 frank frank@zedshaw.com
+./ex17extra dbextra.dat s 3 joe joe@zedshaw.com
+./ex17extra dbextra.dat s 4 Earle Earle@mrbow.com
+./ex17extra dbextra.dat s 5 rancesco rancesco@mrbow.com
+./ex17extra dbextra.dat s 6 illis illis@mrbow.com
+./ex17extra dbextra.dat s 7 oleen oleen@mrbow.com
+./ex17extra dbextra.dat s 8 ewel ewel@mrbow.com
+./ex17extra dbextra.dat s 9 aith aith@mrbow.com
+./ex17extra dbextra.dat s 10 alene alene@mrbow.com
+./ex17extra dbextra.dat s 11 amian amian@mrbow.com
+./ex17extra dbextra.dat s 12 oby oby@mrbow.com
+./ex17extra dbextra.dat s 13 ura ura@mrbow.com
+./ex17extra dbextra.dat s 14 tella tella@mrbow.com
+./ex17extra dbextra.dat s 15 ulu ulu@mrbow.com
+./ex17extra dbextra.dat s 16 emetra emetra@mrbow.com
+./ex17extra dbextra.dat s 17 reanne reanne@mrbow.com
+./ex17extra dbextra.dat s 18 eed eed@mrbow.com
+./ex17extra dbextra.dat s 19 ackelyn ackelyn@mrbow.com
+./ex17extra dbextra.dat s 20 ill ill@mrbow.com
+./ex17extra dbextra.dat s 21 lbina lbina@mrbow.com
+./ex17extra dbextra.dat s 22 ufemia ufemia@mrbow.com
+./ex17extra dbextra.dat s 23 annie annie@mrbow.com
+
+./ex17extra dbextra.dat l
